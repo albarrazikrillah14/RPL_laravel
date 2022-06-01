5fa24cb04296636bd98021dcb8348ea6d5ecb82b
albarrazikrillah14 <72233920+albarrazikrillah14@users.noreply.github.com>
2022-06-01 16:44:29 +0700

    UI capek cui, basket dolo

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,10 +2,17 @@
 @extends('layouts.main')
 
 @section('container')
-<article>
-    <h2>{{ $post->title }}</h2>
-    <p>By: <a href="/authors/{{ $post->author->username}}" class ="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-    {!! $post->body !!}
-</article>
-<a href="/posts" class="d-block mt-3">back to posts</a>
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+            <h1 class="mb-3">{{ $post->title }}</h1>
+            <p>By: <a href="/authors/{{ $post->author->username}}" class ="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+            <img src="https://source.unsplash.com/1200x400?{{ $post -> category -> name}}" class="card-img-top" alt="{{ $post -> category -> name }}">
+            <article class="my-5 fs-5">
+                {!! $post->body !!}
+            </article>
+            <a href="/posts" class="d-block mt-3">back to posts</a>
+        </div>
+    </div>
+</div>
 @endsection
